7c90bfb5ace3e46960709ec5e2d2827bd40b350e
sugayathri <sugayathri100@gmail.com>
2020-06-24 13:31:17 +0530

    multiplication and star pattern

---
+++ simplejavaprogram/src/javapgm/Javaprogram.java
@@ -0,0 +1,9 @@
+package javapgm;
+
+public class Javaprogram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ simplejavaprogram/src/javapgm/Multiplication.java
@@ -0,0 +1,23 @@
+package javapgm;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++)
+		
+		{
+			System.out.print("\n");
+			for(int j=1;j<=5;j++)
+			{
+				
+				int k=i*j;
+				
+				System.out.println(i+ "*"+j+"="+k);
+				
+			}
+		}
+			
+	}
+
+}
